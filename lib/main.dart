e95dbe9bce4864fe1f44178d0af0a6c6d2f1e5eb
Alpha17-2 <alphaisgod1@gmail.com>
2024-10-21 19:29:32 +0530

    [new] : added registration screen and integrated `register api`

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,38 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:provider/provider.dart';
+import 'package:talker_flutter/talker_flutter.dart';
+import 'package:tradefolioo/controllers/auth_controller.dart';
 import 'package:tradefolioo/services/app_routes.dart';
 
 void main(List<String> args) {
-  runApp(TradeFolioo());
+  final talker = TalkerFlutter.init();
+  runZonedGuarded(
+    () => runApp(TradeFolioo(talker: talker)),
+    (Object error, StackTrace stack) {
+      talker.handle(error, stack, 'Uncaught app exception');
+    },
+  );
 }
 
 class TradeFolioo extends StatelessWidget {
-  const TradeFolioo({super.key});
+  final Talker talker;
+  const TradeFolioo({super.key, required this.talker});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'TradeFolioo',
-      
-      getPages: AppRoutes.routes,
-      initialRoute: AppRoutes.splashScreen,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AuthController(),)
+      ],
+      child: GetMaterialApp(
+        title: 'TradeFolioo',
+        
+        getPages: AppRoutes.routes,
+        initialRoute: AppRoutes.splashScreen,
+      ),
     );
   }
 }
